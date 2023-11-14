697fb30304aef1483a9b05ff36eec8434cdc5def
azhararrozak <azhardrozak2001@upi.edu>
2023-11-14 20:53:22 +0700

    Add AR React

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,16 @@ import QuizPernafasan from "./components/Dashboard/QuizPernafasanContent";
 import QuizPenyakit from "./components/Dashboard/QuizPenyakitContent";
 import AugmentedReality from "./components/Dashboard/AugmentedRealityContent";
 import AugmentedRealityMenuContent from "./components/Dashboard/AugmentedRealityMenuContent";
+import ImageTrackingExample from "./components/Augmented/image-tracking-example";
+
 function App() {
   return (
     <div>
       <Toaster position="top-right" toastOptions={{ duration: 2000 }} />
       <Routes>
+        
         <Route path="/" element={<Homepage />} />
+        <Route path="/ar" element={<ImageTrackingExample />} />
         <Route path="/dashboard" element={<Dashboard />}>
           <Route path="overview" element={<DashboardContent />} />{" "}
           {/* Ini adalah rute utama */}
