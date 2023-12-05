8be541b05898e23bbd5f1a67e52c28dcb4630856
azhararrozak <azhardrozak2001@upi.edu>
2023-12-05 14:03:16 +0700

    Add Navbar Responsif

--- src/components/LandingPage/Navbar.jsx
+++ src/components/LandingPage/Navbar.jsx
@@ -1,19 +1,52 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = ({ home, about, programs }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="border-b-2 fixed w-full bg-white">
       <nav className="text-cyan-800 p-6">
-        <div className="flex items-center justify-center text-xs md:justify-center md:text-xl lg:justify-end lg:text-xl">
-          <div className="lg:space-x-8 md:space-x-4  space-x-2 uppercase ">
-            <Link to="/" onClick={home}>
+        <div
+          className={`${
+            isMenuOpen
+              ? "flex flex-col items-center"
+              : "flex"
+          }   justify-center text-xs md:justify-center md:text-xl lg:justify-end lg:text-xl`}
+        >
+          <div className="md:hidden flex">
+            {isMenuOpen ? (
+              <FaTimes
+                className="text-2xl cursor-pointer"
+                onClick={toggleMenu}
+              />
+            ) : (
+              <FaBars
+                className="text-2xl cursor-pointer"
+                onClick={toggleMenu}
+              />
+            )}
+          </div>
+          <div
+            className={`${
+              isMenuOpen ? "flex flex-col" : "hidden"
+            } md:flex items-center space-x-4 uppercase my-4 md:my-0`}
+          >
+            <Link to="/" onClick={home} className="my-2">
               Home
             </Link>
-            <Link onClick={programs}>Programs</Link>
-            <Link onClick={about}>About Us</Link>
-
-            <Link to="/dashboard/overview" className="border-2 px-4 py-2 hover:bg-button">
+            <Link onClick={programs} className="my-2">Programs</Link>
+            <Link onClick={about} className="my-2">About Us</Link>
+            <Link
+              to="/dashboard/overview"
+              className="border-2 px-4 py-2 my-2 hover:bg-button"
+            >
               Mulai Belajar
             </Link>
           </div>
