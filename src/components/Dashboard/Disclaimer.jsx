5229a12cae151769e6af3a1fd7f624069e2b8a7b
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-12-07 20:28:32 +0700

    restructed the lung tech

--- src/components/Dashboard/Disclaimer.jsx
+++ src/components/Dashboard/Disclaimer.jsx
@@ -7,12 +7,14 @@ const Disclaimer = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="max-w-2xl p-6 mb-4 bg-white shadow-md rounded-md">
-        <h1 className="text-2xl font-semibold mb-4 text-center italic">Disclaimer</h1>
+        <h1 className="text-2xl font-semibold mb-4 text-center italic">
+          Disclaimer
+        </h1>
         <p className="text-gray-700 mb-4">
-          Media pembelajaran ini disusun oleh Johannes, Azhar, dan Hafil sebagai
-          bagian dari proyek pembelajaran. Materi dan informasi yang terdapat
-          dalam media ini disusun dengan seksama berdasarkan penelitian dan
-          referensi yang relevan pada saat penyusunan.
+          Media pembelajaran ini disusun oleh The Lungs Tech sebagai bagian dari
+          proyek pembelajaran. Materi dan informasi yang terdapat dalam media
+          ini disusun dengan seksama berdasarkan penelitian dan referensi yang
+          relevan pada saat penyusunan.
         </p>
         <p className="text-gray-700 mb-4">
           Pembaca diizinkan untuk mengutip atau menggunakan informasi yang
@@ -32,18 +34,17 @@ const Disclaimer = () => {
           yang terdapat dalam media pembelajaran ini. Penggunaan informasi
           adalah tanggung jawab penuh pembaca.
         </p>
-        
+
         <div className="w-full justify-center items-center  flex my-4">
-      <button
-        className="bg-button text-black border font-bold py-2 px-4 rounded"
-        onClick={handleNextClick}
-      >
-        {" "}
-        Next<span className="ml-2">&#8594;</span>
-      </button>
+          <button
+            className="bg-button text-black border font-bold py-2 px-4 rounded"
+            onClick={handleNextClick}
+          >
+            {" "}
+            Next<span className="ml-2">&#8594;</span>
+          </button>
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
--- src/components/LandingPage/Footer.jsx
+++ src/components/LandingPage/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
-    return (
-      <footer className="bg-black text-white py-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; 2023 Kelompok 1 Kapita Selekta. All rights reserved.</p>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <footer className="bg-black text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2023 The Lungs Tech All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
