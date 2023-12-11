66e70c7ca4efa6a6f5b6ad36e36a3af88356362d
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-12-11 13:37:13 +0700

    ok

--- src/components/Dashboard/Data/SistemPernafasanMateri.js
+++ src/components/Dashboard/Data/SistemPernafasanMateri.js
@@ -1,87 +1,87 @@
 const SistemPernafasanMateri = [
-    {
-      id: 1,
-      judul: "Pengantar Pada Materi Sistem Organ Pernafasan",
-      image: "",
-      paragraphs: [
-        "Setiap makhluk hidup terutama manusia membutuhkan oksigen untuk tetap bertahan hidup. Oksigen yang dihirup akan diolah oleh sistem pernafasan lalu dibuang menjadi karbon dioksida. Proses pernafasan ini terus dilakukan berulang ulang sampai ujung makhluk hidup. Sistem pernafasan sendiri adalah serangkaian organ yang memungkinkan manusia untuk bernafas dan mengambil oksigen dari udara. Sistem ini melibatkan organ-organ seperti paru-paru, bronkus, dan diafragma.",
-        "Mengapa perlu bernafas?",
-        "Tubuh kalian membutuhkan oksigen untuk melakukan respirasi seluler. Respirasi seluler adalah suatu proses yang terjadi di dalam sel, saat glukosa dari makanan yang kalian makan dipecah dengan bantuan oksigen sehingga menghasilkan energi. Selama proses respirasi seluler, karbondioksida dan air menjadi sisa hasil respirasi sel yang dibuang ketika kalian menghembuskan napas. Energi yang dihasilkan digunakan oleh kalian untuk beraktivitas.",
-      ],
-    },
-    {
-      id: 2,
-      judul: "Sistem Organ Pernafasan",
-      image: "/1_1.PNG",
-      paragraphs: [
-        "Hidung",
-        "Pertama, udara masuk ke tubuh melalui hidung dan mulut. Rambut-rambut di dalam hidung berfungsi untuk menangkap partikel besar yang masuk bersama udara. Udara kemudian masuk ke dalam rongga hidung, di mana sel-sel menghasilkan lendir (mukus) untuk melembabkan udara dan menangkap lebih banyak partikel.",
-      ],
-    },
-    {
-      id: 3,
-      judul: "Sistem Organ Pernafasan",
-      image: "/1_2.PNG",
-      paragraphs: [
-        "Faring",
-        "Setelah Udara melewati hidung, udara menuju faring yang terhubung dengan tenggorokan. Kedua saluran ini terhubung dengan baik, sehingga udara dan makanan dapat masuk ke faring. Faring merupakan saluran sekitar 12,5–13 cm yang merupakan kelanjutan dari saluran hidung dan meneruskan udara ke laring. Faring terletak di antara saluran pernapasan dan saluran pencernaan.",
-        "Laring",
-        "Laring terdiri dari lempengan-lempengan tulang rawan. Dinding bagian dalam laring dapat digerakkan oleh otot untuk membuka dan menutup glotis, yang merupakan lubang/celah yang menghubungkan trakea dengan faring. Pada saat menelan makanan, laring terangkat sehingga anak tekak menutup rongga glotis (rongga di antara pita suara), mencegah makanan masuk ke trakea. Otot-otot laring juga dapat menggerakkan pita suara.",
-        "Trakea",
-        "Trakea adalah saluran berongga dengan dinding terdiri dari cincin-cincin tulang rawan. Trakea dilengkapi dengan otot polos yang menjaga agar bronkus tidak menyempit saat bernapas. Trakea juga memiliki lendir dan silia untuk menyaring debu dan bakteri, mencegah mereka masuk ke paru-paru Ketika manusia menelan makanan, epiglotis menutup trakea untuk mencegah makanan masuk ke saluran napas. Sel yang melapisi trakea memiliki silia yang menggerakkan lendir yang menangkap kotoran dari trakea ke faring. Dengan batuk, kotoran tersebut terlontar.",
-      ],
-    },
-    {
-      id: 4,
-      judul: "Sistem Organ Pernafasan",
-      image: "/1_3.PNG",
-      paragraphs: [
-        "Bronkus dan Paru-Paru",
-        "Setelah melewati trakea, udara mencapai bronkus. Bronkus merupakan cabang-cabang dari trakea yang mengarah ke paru-paru kiri dan paru-paru kanan. Paru-paru sendiri adalah organ berpasangan yang terletak di dalam rongga dada. Mereka berperan sebagai pusat utama pertukaran gas, di mana oksigen dan karbon dioksida diserap dan dilepaskan. Paru-paru kanan memiliki tiga gelambir, sementara paru-paru kiri memiliki dua gelambir. Untuk melindungi paru-paru dari gesekan saat mengembang dan mengempis, paru-paru dilapisi oleh selaput rangkap yang disebut pleura. Antara selaput rangkap ini terdapat cairan yang berfungsi sebagai pelumas. ",
-        "Bronkus bercabang menjadi bagian yang semakin kecil, membentuk bronkiolus. Pada bronkiolus yang paling ujung, terdapat gelembung-gelembung kecil yang disebut alveoli, tempat terjadinya pertukaran gas antara darah dan udara. Oksigen dari udara berdifusi ke dalam darah, sementara karbon dioksida dan uap air dari darah berdifusi ke dalam udara.",
-      ],
-    },
-    {
-      id: 5,
-      judul: "Proses Pernafasan",
-      image: "/1_4.PNG",
-      paragraphs: [
-        "Sama seperti pergerakan tubuh manusia lainnya, bernapas juga dikendalikan oleh otot. Paru-paru dilindungi oleh tulang rusuk yang ditempeli oleh otot-otot di sekitarnya. Di bagian bawah paru-paru juga terdapat otot yang besar, kuat, dan berbentuk seperti kubah. Otot tersebut dinamakan diafragma.Ketika kalian menarik napas, otot yang ada di tulang rusuk kalian akan berkontraksi sehingga tulang rusuk akan naik ke atas, diafragma pun berkontraksi, yang semula melengkung menjadi mendatar. Kontraksi ini menyebabkan rongga dada kalian mengembang, volume paru-paru pun membesar, tekanan udara di paru-paru mengecil, dan udara pun masuk. Ketika kalian menghembuskan napas, otot-otot yang ada di tulang rusuk kalian rileks atau dalam keadaan santai, diafragma pun dalam keadaan rileks, hal itu menyebabkan rongga dada kalian menjadi lebih kecil, volume udara di paru-paru pun berkurang, sementara tekanan udara di paru-paru membesar, udara siap untuk dikeluarkan.",
-      ],
-    },
-    {
-      id: 6,
-      judul: "Proses Pernafasan",
-      image: "",
-      paragraphs: [
-        "Proses masuk (inspirasi) dan keluarnya udara (ekspirasi) dalam pernapasan berkaitan erat dengan perbedaan volume dan tekanan udara. Proses inspirasi dan ekspirasi diatur oleh kerja otot-otot diafragma dan otot-otot antartulang rusuk. Ada dua jenis mekanisme pernapasan dalam manusia, yaitu pernapasan perut dan pernapasan dada.",
-      ],
-    },
-    {
-      id: 7,
-      judul: "Pernafasan Dada",
-      image: "/1_6.png",
-      paragraphs: [
-        "Otot-otot antartulang rusuk mengalami kontraksi, mengakibatkan peningkatan volume rongga dada dan tekanan udara dalam paru-paru menjadi lebih rendah daripada tekanan udara di atmosfer. Hal ini memungkinkan udara luar masuk ke dalam paru-paru (inspirasi). Ketika otot-otot antartulang rusuk rileks, volume rongga dada berkurang, meningkatkan tekanan udara dalam paru-paru, dan udara keluar (ekspirasi).",
-      ],
-    },
-    {
-      id: 8,
-      judul: "Pernafasan Perut",
-      image: "/1_5.JPG",
-      paragraphs: [
-        "Otot diafragma mengalami kontraksi, membuat diafragma datar dan meningkatkan volume rongga dada. Hal ini mengakibatkan tekanan udara dalam paru-paru lebih rendah dari tekanan udara luar, memungkinkan udara masuk (inspirasi). Ketika otot diafragma rileks, diafragma melengkung ke atas dan volume rongga dada berkurang, meningkatkan tekanan udara dalam paru-paru, dan udara keluar (ekspirasi).",
-      ],
-    },
-    {
-      id: 9,
-      judul: "Penutup",
-      image: "",
-      paragraphs: [
-        "Darah membawa oksigen dan karbon dioksida ke dan dari sel-sel tubuh. Pertukaran ini terjadi di alveoli, di mana oksigen dari udara yang dihirup masuk ke dalam darah dan karbon dioksida dibuang dari darah ke dalam udara. Proses ini disebut pertukaran gas. Kapiler dan dinding alveolus sangat tipis, memungkinkan zat-zat tersebut untuk saling berpindah. Karena peran penting sistem pernapasan ini, menjaganya dengan baik sangat diperlukan agar tetap sehat.",
-      ],
-    },
-    // Tambahkan data sesuai kebutuhan
-  ];
+  {
+    id: 1,
+    judul: "Pengantar Pada Materi Sistem Organ Pernafasan",
+    image: "",
+    paragraphs: [
+      "Setiap makhluk hidup terutama manusia membutuhkan oksigen untuk tetap bertahan hidup. Oksigen yang dihirup akan diolah oleh sistem pernafasan lalu dibuang menjadi karbon dioksida. Proses pernafasan ini terus dilakukan berulang ulang sampai ujung makhluk hidup. Sistem pernafasan sendiri adalah serangkaian organ yang memungkinkan manusia untuk bernafas dan mengambil oksigen dari udara. Sistem ini melibatkan organ-organ seperti paru-paru, bronkus, dan diafragma.",
+      "Mengapa perlu bernafas?",
+      "Tubuh kalian membutuhkan oksigen untuk melakukan respirasi seluler. Respirasi seluler adalah suatu proses yang terjadi di dalam sel, saat glukosa dari makanan yang kalian makan dipecah dengan bantuan oksigen sehingga menghasilkan energi. Selama proses respirasi seluler, karbondioksida dan air menjadi sisa hasil respirasi sel yang dibuang ketika kalian menghembuskan napas. Energi yang dihasilkan digunakan oleh kalian untuk beraktivitas.",
+    ],
+  },
+  {
+    id: 2,
+    judul: "Sistem Organ Pernafasan",
+    image: "/1_1.PNG",
+    paragraphs: [
+      "Hidung",
+      "Pertama, udara masuk ke tubuh melalui hidung dan mulut. Rambut-rambut di dalam hidung berfungsi untuk menangkap partikel besar yang masuk bersama udara. Udara kemudian masuk ke dalam rongga hidung, di mana sel-sel menghasilkan lendir (mukus) untuk melembabkan udara dan menangkap lebih banyak partikel.",
+    ],
+  },
+  {
+    id: 3,
+    judul: "Sistem Organ Pernafasan",
+    image: "/1_2.PNG",
+    paragraphs: [
+      "Faring",
+      "Setelah Udara melewati hidung, udara menuju faring yang terhubung dengan tenggorokan. Kedua saluran ini terhubung dengan baik, sehingga udara dan makanan dapat masuk ke faring. Faring merupakan saluran sekitar 12,5–13 cm yang merupakan kelanjutan dari saluran hidung dan meneruskan udara ke laring. Faring terletak di antara saluran pernapasan dan saluran pencernaan.",
+      "Laring",
+      "Laring terdiri dari lempengan-lempengan tulang rawan. Dinding bagian dalam laring dapat digerakkan oleh otot untuk membuka dan menutup glotis, yang merupakan lubang/celah yang menghubungkan trakea dengan faring. Pada saat menelan makanan, laring terangkat sehingga anak tekak menutup rongga glotis (rongga di antara pita suara), mencegah makanan masuk ke trakea. Otot-otot laring juga dapat menggerakkan pita suara.",
+      "Trakea",
+      "Trakea adalah saluran berongga dengan dinding terdiri dari cincin-cincin tulang rawan. Trakea dilengkapi dengan otot polos yang menjaga agar bronkus tidak menyempit saat bernapas. Trakea juga memiliki lendir dan silia untuk menyaring debu dan bakteri, mencegah mereka masuk ke paru-paru Ketika manusia menelan makanan, epiglotis menutup trakea untuk mencegah makanan masuk ke saluran napas. Sel yang melapisi trakea memiliki silia yang menggerakkan lendir yang menangkap kotoran dari trakea ke faring. Dengan batuk, kotoran tersebut terlontar.",
+    ],
+  },
+  {
+    id: 4,
+    judul: "Sistem Organ Pernafasan",
+    image: "/1_3.PNG",
+    paragraphs: [
+      "Bronkus dan Paru-Paru",
+      "Setelah melewati trakea, udara mencapai bronkus. Bronkus merupakan cabang-cabang dari trakea yang mengarah ke paru-paru kiri dan paru-paru kanan. Paru-paru sendiri adalah organ berpasangan yang terletak di dalam rongga dada. Mereka berperan sebagai pusat utama pertukaran gas, di mana oksigen dan karbon dioksida diserap dan dilepaskan. Paru-paru kanan memiliki tiga gelambir, sementara paru-paru kiri memiliki dua gelambir. Untuk melindungi paru-paru dari gesekan saat mengembang dan mengempis, paru-paru dilapisi oleh selaput rangkap yang disebut pleura. Antara selaput rangkap ini terdapat cairan yang berfungsi sebagai pelumas. ",
+      "Bronkus bercabang menjadi bagian yang semakin kecil, membentuk bronkiolus. Pada bronkiolus yang paling ujung, terdapat gelembung-gelembung kecil yang disebut alveoli, tempat terjadinya pertukaran gas antara darah dan udara. Oksigen dari udara berdifusi ke dalam darah, sementara karbon dioksida dan uap air dari darah berdifusi ke dalam udara.",
+    ],
+  },
+  {
+    id: 5,
+    judul: "Proses Pernafasan",
+    image: "/1_4.PNG",
+    paragraphs: [
+      "Sama seperti pergerakan tubuh manusia lainnya, bernapas juga dikendalikan oleh otot. Paru-paru dilindungi oleh tulang rusuk yang ditempeli oleh otot-otot di sekitarnya. Di bagian bawah paru-paru juga terdapat otot yang besar, kuat, dan berbentuk seperti kubah. Otot tersebut dinamakan diafragma.Ketika kalian menarik napas, otot yang ada di tulang rusuk kalian akan berkontraksi sehingga tulang rusuk akan naik ke atas, diafragma pun berkontraksi, yang semula melengkung menjadi mendatar. Kontraksi ini menyebabkan rongga dada kalian mengembang, volume paru-paru pun membesar, tekanan udara di paru-paru mengecil, dan udara pun masuk. Ketika kalian menghembuskan napas, otot-otot yang ada di tulang rusuk kalian rileks atau dalam keadaan santai, diafragma pun dalam keadaan rileks, hal itu menyebabkan rongga dada kalian menjadi lebih kecil, volume udara di paru-paru pun berkurang, sementara tekanan udara di paru-paru membesar, udara siap untuk dikeluarkan.",
+    ],
+  },
+  {
+    id: 6,
+    judul: "Proses Pernafasan",
+    image: "/paru.gif",
+    paragraphs: [
+      "Proses masuk (inspirasi) dan keluarnya udara (ekspirasi) dalam pernapasan berkaitan erat dengan perbedaan volume dan tekanan udara. Proses inspirasi dan ekspirasi diatur oleh kerja otot-otot diafragma dan otot-otot antartulang rusuk. Ada dua jenis mekanisme pernapasan dalam manusia, yaitu pernapasan perut dan pernapasan dada.",
+    ],
+  },
+  {
+    id: 7,
+    judul: "Pernafasan Dada",
+    image: "/1_6.png",
+    paragraphs: [
+      "Otot-otot antartulang rusuk mengalami kontraksi, mengakibatkan peningkatan volume rongga dada dan tekanan udara dalam paru-paru menjadi lebih rendah daripada tekanan udara di atmosfer. Hal ini memungkinkan udara luar masuk ke dalam paru-paru (inspirasi). Ketika otot-otot antartulang rusuk rileks, volume rongga dada berkurang, meningkatkan tekanan udara dalam paru-paru, dan udara keluar (ekspirasi).",
+    ],
+  },
+  {
+    id: 8,
+    judul: "Pernafasan Perut",
+    image: "/1_5.JPG",
+    paragraphs: [
+      "Otot diafragma mengalami kontraksi, membuat diafragma datar dan meningkatkan volume rongga dada. Hal ini mengakibatkan tekanan udara dalam paru-paru lebih rendah dari tekanan udara luar, memungkinkan udara masuk (inspirasi). Ketika otot diafragma rileks, diafragma melengkung ke atas dan volume rongga dada berkurang, meningkatkan tekanan udara dalam paru-paru, dan udara keluar (ekspirasi).",
+    ],
+  },
+  {
+    id: 9,
+    judul: "Penutup",
+    image: "",
+    paragraphs: [
+      "Darah membawa oksigen dan karbon dioksida ke dan dari sel-sel tubuh. Pertukaran ini terjadi di alveoli, di mana oksigen dari udara yang dihirup masuk ke dalam darah dan karbon dioksida dibuang dari darah ke dalam udara. Proses ini disebut pertukaran gas. Kapiler dan dinding alveolus sangat tipis, memungkinkan zat-zat tersebut untuk saling berpindah. Karena peran penting sistem pernapasan ini, menjaganya dengan baik sangat diperlukan agar tetap sehat.",
+    ],
+  },
+  // Tambahkan data sesuai kebutuhan
+];
 
-  export default SistemPernafasanMateri;
+export default SistemPernafasanMateri;
